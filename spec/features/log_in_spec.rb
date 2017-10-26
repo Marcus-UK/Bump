b232026c1412001d8d61c40630f789ec87b9c200
Joe Ingram <josephcingram@hotmail.co.uk>
2017-10-26 12:44:28 +0100

    Add new test for login and ammend broken places test

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -7,3 +7,16 @@ feature 'reject if not signed up' do
     expect(page).to have_content('Invalid Email or password')
   end
 end
+
+feature 'succesfully sign in if user signed up' do
+  scenario 'sign up, go to log_in page and be allowed to log in' do
+    sign_up
+    p page.body
+    click_link 'Logout'
+    click_link 'Login'
+    fill_in 'Email', with: 'gi_joeuk@hotmail.com'
+    fill_in 'Password', with: '123345'
+    click_button 'Log in'
+    expect(page).to have_content("Signed in successfully.")
+  end
+end
