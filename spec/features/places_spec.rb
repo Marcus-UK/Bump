31178b09e0685aed89c1e9d4fbc8ef53c6636afc
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-10-27 14:51:14 +0100

    fixing styles and functionality

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -5,7 +5,7 @@ feature 'check places class' do
     fill_in 'place[title]', with: 'Makers Academy'
     fill_in 'place[address]', with: '50 Commercial Street'
     click_button 'Add!'
-    expect(page).to have_content('Makers Academy')
+    expect(page).to have_content('50 Commercial Street')
   end
 end
 
